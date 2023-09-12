f87ebb56a283fc91b7c65227e6a471c197bca2c1
Valentina Valverde <vvgreenville@gmail.com>
2023-09-11 22:43:01 -0400

    Tested out some things in JavaScript.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,8 +17,54 @@ const pushNum = (a) => {
     console.log(a);
 }
 
-button.forEach((b) => b.addEventListener('click', function(){
-    pushNum(numZero);
+//my goal here is option 1: if the value of the button clicked is = to *certain value*, return certain value.
+//option 2: call number when clicked
+
+button.forEach((b) => b.addEventListener('click', function () {
+    if (value = 0) {
+        pushNum(numZero);
+    }
+
 }))
 
 
+//goal: let the arr nums = the value of the buttons clicked.
+const numbers = [numOne, numFive];
+numbers.push(numTwo);
+
+
+//goal: call each respective function when the button is clicked. add event listener?
+//addition for loop
+let sum = 0;
+
+for (i=0; i<numbers.length; i++){
+    sum += numbers[i];
+}
+console.log(sum);
+
+
+//subtration for loop
+let difference = 0;
+
+for (i=0; i<numbers.length; i++){
+    difference -= numbers[i];
+}
+console.log(difference);
+
+
+//division for loop
+let solution = 100;
+
+for (i=0; i<numbers.length; i++){
+    solution /= numbers[i];
+}
+console.log(solution);
+
+
+//multiplication for loop
+let product = 100;
+
+for (i=0; i<numbers.length; i++){
+    product *= numbers[i];
+}
+console.log(product);
