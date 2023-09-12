a27eec4809148428c5f69f193eb696fbdeb72bd8
Valentina Valverde <vvgreenville@gmail.com>
2023-09-12 16:13:19 -0400

    Finished up the alerts and created for loops for the math functions.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,19 +13,86 @@ const numZero = document.getElementById('numZero').value;
 
 const button = document.querySelectorAll('.button');
 
-const pushNum = (a) => {
-    console.log(a);
-}
 
 //my goal here is option 1: if the value of the button clicked is = to *certain value*, return certain value.
 //option 2: call number when clicked
+//assign values to buttons...?
 
-button.forEach((b) => b.addEventListener('click', function () {
-    if (value = 0) {
-        pushNum(numZero);
-    }
 
-}))
+button.forEach((x) => {
+    x.addEventListener('click', ()=> {
+        const numValue = x.value;
+        console.log(numValue);
+        switch (numValue){
+            case "1":
+                alert(numValue);
+                break;
+            case "2":
+                alert(numValue);
+                break;
+            case "3":
+                alert(numValue);
+                break;
+            case "4":
+                alert(numValue);
+                break;
+            case "5":
+                alert(numValue);
+                break;
+            case "6":
+                alert(numValue);
+                break;
+            case "7":
+                alert(numValue);
+                break;
+            case "8":
+                alert(numValue);
+                break;
+            case "9":
+                alert(numValue);
+                break;
+            case "0":
+                alert(numValue);
+                break;
+
+
+            case "+":
+                alert(numValue);
+                break;
+            case "-":
+                alert(numValue);
+                break;
+            case "/":
+                alert(numValue);
+                break;
+            case "*":
+                alert(numValue);
+                break;
+            case "+/-":
+                alert(numValue);
+                break;
+            case "%":
+                alert(numValue);
+                break;
+            case "clear":
+                alert(numValue);
+                break;
+
+
+            case "=":
+                alert(numValue);
+                break;
+
+            default:
+                console.log("nada");
+                break;
+        }
+    })
+})
+
+const calculation = [];
+
+
 
 
 //goal: let the arr nums = the value of the buttons clicked.
@@ -68,3 +135,22 @@ for (i=0; i<numbers.length; i++){
     product *= numbers[i];
 }
 console.log(product);
+
+
+
+
+
+
+
+
+//Isaac tried helping but now I'm more confused
+const pushNum = (a) => {
+    console.log(a);
+}
+
+button.forEach((b) => b.addEventListener('click', function () {
+    if (value = 0) {
+        pushNum(numZero);
+    }
+
+}))
